226f8918bb8621e4c5818f6a05d2ac1083ee748a
Mrinalini Nair Ani <91671136+hacksh4w@users.noreply.github.com>
2023-07-01 19:17:19 +0530

    Create xor_tb.v

---
+++ BASIC GATES/XOR GATE/xor_tb.v
@@ -0,0 +1,15 @@
+module xor_tb;
+reg a,b;
+wire y;
+
+xor_gate_s uut(a,b,y);
+
+initial 
+    begin
+        a = 1'b0; b = 1'b0;#10
+        b = 1'b0; b = 1'b1;#10
+        a = 1'b1; b = 1'b0;#10
+        b = 1'b1; b = 1'b1;#10
+        $finish();
+    end
+endmodule
